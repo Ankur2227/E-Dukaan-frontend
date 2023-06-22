614a7314f89e1cf9cd8689e3bd0abb03912b7b11
Ankur Kumar <ankur4e@gmail.com>
2023-06-22 22:43:49 +0530

    frontend-done

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,41 @@
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 import "./CartItem.scss";
 const CartItem = () => {
-    return <div>Cart item</div>;
+    
+    return (
+        <div className="cart-products">
+            <div className="search-result-item">
+                <div className="image-container">
+                    <img src={prod} alt=""/>
+                </div>
+                <div className="prod-details">
+                    <span className="name">titeifjdsjsldfhlkdfnlksfnlkdfnfeofjosafjpwoafjole</span>
+                    <MdClose
+                        className="close-btn"
+                    />
+                        <div className="quantity-buttons">
+                            <span>
+                                -
+                            </span>
+                            <span>quantity</span>
+                            <span>
+                                +
+                            </span>
+                        </div>
+                        <div className="text">
+                            <span>quantity</span>
+                            <span>x</span>
+                            <span className="highlight">
+                                <span>&#8377;</span>
+                            </span>
+                        </div>
+                    </div>
+                </div>
+
+        </div>
+    );
 };
 
 export default CartItem;
--- src/components/Header/Search/Search.jsx
+++ src/components/Header/Search/Search.jsx
@@ -1,6 +1,39 @@
+import React, { useState } from "react";
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/earbuds-prod-1.webp";
+
 import "./Search.scss";
-const Search = () => {
-    return <div>Search</div>;
+
+const Search = ({ setShowSearch }) => {
+    return (
+        <div className="search-modal">
+            <div className="form-field">
+                <input
+                    autoFocus
+                    type="text"
+                    placeholder="Search for products"
+                />
+                <MdClose
+                    className="close-btn"
+                    onClick={() => setShowSearch(false)}
+                />
+            </div>
+            <div className="search-result-content">
+                <div className="search-results">
+                    <div className="search-result-item">
+                        <div className="image-container">
+                            <img src={prod} alt=""/>
+                        </div>
+                        <div className="prod-details">
+                            <span className="name">titeifjds</span>
+                            <span className="desc">sfjcnlks</span>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 };
 
 export default Search;
