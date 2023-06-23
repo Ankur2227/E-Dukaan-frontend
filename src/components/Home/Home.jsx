7af26e611bcc80acccfab6df8da36c19a4dd3d16
Ankur Kumar <ankur4e@gmail.com>
2023-06-23 15:04:24 +0530

    api integration done. data fetching in backend.

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,18 +2,44 @@ import "./Home.scss";
 import Banner from "./Banner/Banner";
 import Category from "./Category/Category.jsx";
 import Products from "../Products/Products";
+import { fetchDataFromApi } from "../../utils/api";
+import { useEffect,useContext } from "react";
+import { Context } from "../../utils/context";
 
 const Home = () => {
-    return <div className="home">
-        <Banner/>
-        <div className="main-content">
-            <div className="layout">
-                <Category/>
-                <Products headingText="Popular Products" />
+
+    const { products, setProducts, categories, setCategories } =
+        useContext(Context);
+    useEffect(() => {
+        getProducts();
+        getCategories();
+    }, []);
+
+    const getProducts = () => {
+        fetchDataFromApi("/api/products?populate=*").then((res) => {
+            setProducts(res);
+        });
+    };
+    const getCategories = () => {
+        fetchDataFromApi("/api/categories?populate=*").then((res) => {
+            setCategories(res);
+        });
+    };
+
+    return (
+        <div>
+            <Banner />
+            <div className="main-content">
+                <div className="layout">
+                    <Category categories={categories} />
+                    <Products
+                        headingText="Popular Products"
+                        products={products}
+                    />
+                </div>
             </div>
         </div>
-        
-    </div>;
+    );
 };
 
 export default Home;
