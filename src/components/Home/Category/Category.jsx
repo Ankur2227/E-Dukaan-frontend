0b9643816d99926b66169960c0dc2460725ba270
Ankur Kumar <ankur4e@gmail.com>
2023-06-23 15:39:07 +0530

    category and products fetching through api.

--- src/components/Home/Category/Category.jsx
+++ src/components/Home/Category/Category.jsx
@@ -1,21 +1,19 @@
 import "./Category.scss";
 import cat1 from "../../../assets/category/cat-1.jpg";
 
-const Category = () => {
+const Category = ({categories}) => {
     return <div className="shop-by-category">
-        <div className="categories">
-            <div className="category">
+        <div className="categories">   
+            {categories.data.map((item)=>{
+                return (
+                    <div className="category">
+                        <img src={process.env.REACT_APP_DEV_URL + item.attributes.img.data.attributes.url} alt=""/>
+                    </div>
+                )
+            })}
+            {/* <div className="category">
                 <img src={cat1} alt=""/>
-            </div>
-            <div className="category">
-                <img src={cat1} alt=""/>
-            </div>
-            <div className="category">
-                <img src={cat1} alt=""/>
-            </div>
-            <div className="category">
-                <img src={cat1} alt=""/>
-            </div>
+            </div> */}
         </div>
     </div>;
 };
--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,18 +1,25 @@
 import "./Products.scss";
 import Product from "./Product/Product.jsx";
 
-const Products = ({headingText}) => {
+const Products = ({products,headingText}) => {
     return <div className="products-container">
         <div className="sec-heading">{headingText}</div>
         <div className="products">
+            {products.data.map((item)=>
+                <Product
+                    key={item.id}
+                    id={item.id}
+                    data={item.attributes}
+                />
+            )}
+
+            {/* <Product/>
             <Product/>
             <Product/>
             <Product/>
             <Product/>
             <Product/>
-            <Product/>
-            <Product/>
-            <Product/>
+            <Product/> */}
         </div>
     </div>;
 };
